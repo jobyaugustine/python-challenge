ec0c1ed7f4a70534ee282b82b31f3caaea6d9143
joby <jobyp_a@yahoo.com>
2021-09-28 12:26:03 -0400

    Refactored logic

--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -1,8 +1,31 @@
 import csv ,operator
-from itertools import groupby
+
+def displayResult(sorted_result, count_votes):
+    with open('/Users/jobyaugustine/Desktop/Assignments/MyAssignmentRepos/python-challenge/PyPoll/Analysis/result.txt','w') as f:
+        f.write("Election Results\n----------------\n")
+        f.write(f'Total Votes : {count_votes} \n------------------------\n')
+    
+        print(sorted_result,count_votes)
+        print("Election Results\n----------------")
+        print(f'Total Votes : {count_votes} \n------------------------')
+        
+        for key, value in sorted_result:
+         percent_candidate = (value/count_votes) *100
+         print(f'{key} : {round(percent_candidate,2)} % ({value})')
+         f.write(f'{key} : {round(percent_candidate,2)} % ({value})\n')
+        print("---------------------------")
+        f.write("---------------------------\n")
+        print(f'Winner: {sorted_result[0][0]}')
+        f.write(f'Winner: {sorted_result[0][0]}\n')
+        print(" ---------------------------")
+        f.write(" ---------------------------\n")
+        return
+
+       
 with open("/Users/jobyaugustine/Desktop/Assignments/MyAssignmentRepos/python-challenge/PyPoll/Resources/election_data.csv","r") as pypoll_file:
     csv_reader=csv.reader(pypoll_file,delimiter=',')
-    next(csv_reader)  # moving to the first row , skipping the headers
+    # moving to the first row , skipping the headers
+    next(csv_reader)  
     # sort data on the basis of candidate
     csv_reader= sorted(csv_reader, key=operator.itemgetter(2))    
     count_votes=0
@@ -10,7 +33,6 @@ with open("/Users/jobyaugustine/Desktop/Assignments/MyAssignmentRepos/python-cha
     for line in csv_reader:
         sorted_candidate_list.append(line[2])
         count_votes= count_votes + 1
-    print(len(sorted_candidate_list))
     
     dict_result = {}
 
@@ -24,7 +46,6 @@ with open("/Users/jobyaugustine/Desktop/Assignments/MyAssignmentRepos/python-cha
                 totCandidateCnt += 1
                 
             else:
-                print(sorted_candidate_list[current],totCandidateCnt) 
                 dict_result.update({sorted_candidate_list[current]:totCandidateCnt})   
                 totCandidateCnt = 1
                 
@@ -32,24 +53,13 @@ with open("/Users/jobyaugustine/Desktop/Assignments/MyAssignmentRepos/python-cha
         next +=1
         current +=1  
         counter += 1  
-    #print(sorted_candidate_list[current-2],totCandidateCnt)  
+
     dict_result.update({sorted_candidate_list[current-2]:totCandidateCnt})  
-    print(dict_result)
-    sorted_result = sorted(dict_result.items(), key=operator.itemgetter(1),reverse=True)    
-    print(sorted_result)
-    print("Election Results\n ----------------")
-    print(f'Total Votes : {count_votes} \n ------------------------')
-    for key, value in sorted_result:
-         #print(key,value)
-         percent_candidate = (value/count_votes) *100
-         print(f'{key} : {round(percent_candidate,2)} % ({value})')
-    print(" ---------------------------")
-    print(f'Winner: {sorted_result[0][0]}')
-    print(" ---------------------------")
+    sorted_result = sorted(dict_result.items(), key=operator.itemgetter(1),reverse=True) 
 
-#writing the result to a text file
-# with open('/Users/jobyaugustine/Desktop/Assignments/MyAssignmentRepos/python-challenge/PyPoll/Analysis/result.txt','w') as f:
-#     f.write(final_result)
+    # to display results and write to result file       
+    displayResult(sorted_result,count_votes)
 
 
-# 
+
+
